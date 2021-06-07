edb7cda534127fb045b020f801c7266d8aa49a52
Sean Maas <seanmaas27@gmail.com>
2021-06-06 23:31:18 -0400

    Vita: add pause menu and save type selector

--- src/vita/main.cpp
+++ src/vita/main.cpp
@@ -57,7 +57,11 @@ int showFpsCounter = 0;
 uint32_t confirmButton, cancelButton;
 vita2d_pgf *pgf;
 
+std::string ndsPath, gbaPath;
 Core *core;
+
+bool running = false;
+std::thread *coreThread, *audioThread, *saveThread;
 
 ScreenLayout layout;
 uint32_t framebuffer[256 * 192 * 2];
@@ -69,13 +73,13 @@ int audioPort = 0;
 void runCore()
 {
     // Run the emulator
-    while (true)
+    while (running)
         core->runFrame();
 }
 
 void outputAudio()
 {
-    while (true)
+    while (running)
     {
         // The NDS sample rate is 32768Hz, but the Vita doesn't support this, so 48000Hz is used
         // Get 699 samples at 32768Hz, which is equal to approximately 1024 samples at 48000Hz
@@ -92,7 +96,7 @@ void outputAudio()
 
 void checkSave()
 {
-    while (true)
+    while (running)
     {
         // Check save files every second and update them if changed
         sceKernelDelayThread(1000000);
@@ -100,12 +104,129 @@ void checkSave()
     }
 }
 
+void startCore()
+{
+    running = true;
+
+    // Start the threads
+    coreThread  = new std::thread(runCore);
+    audioThread = new std::thread(outputAudio);
+    saveThread  = new std::thread(checkSave);
+}
+
+void stopCore()
+{
+    running = false;
+
+    // Wait for the threads to stop
+    coreThread->join();
+    delete coreThread;
+    audioThread->join();
+    delete audioThread;
+    saveThread->join();
+    delete saveThread;
+}
+
+uint32_t menu(std::string title, std::string subtitle, std::vector<std::string> *items, unsigned int *selection, uint32_t buttonMask)
+{
+    // Ignore any buttons that were already pressed
+    uint32_t buttons = 0xFFFFFFFF;
+
+    while (true)
+    {
+        vita2d_start_drawing();
+        vita2d_clear_screen();
+
+        // Draw the title
+        vita2d_pgf_draw_text(pgf, 5, 20, COLOR_TEXT1, 1.0f, title.c_str());
+
+        // If there's a subtitle, draw it and offset the item list
+        unsigned int y = 60;
+        if (subtitle != "")
+        {
+            vita2d_pgf_draw_text(pgf, 5, 40, COLOR_TEXT2, 1.0f, subtitle.c_str());
+            y = 80;
+        }
+
+        // Draw the menu items, highlighting the current selection
+        for (size_t i = 0; i < items->size(); i++)
+            vita2d_pgf_draw_text(pgf, 5, y + i * 20, (*selection == i ? COLOR_TEXT3 : COLOR_TEXT1), 1.0f, (*items)[i].c_str());
+
+        vita2d_end_drawing();
+        vita2d_swap_buffers();
+
+        // Scan for newly-pressed buttons
+        SceCtrlData held;
+        sceCtrlPeekBufferPositive(0, &held, 1);
+        uint32_t pressed = held.buttons & ~buttons;
+        buttons = held.buttons;
+
+        // Handle menu input
+        if (pressed & buttonMask)
+        {
+            // Return the pressed buttons so they can be handled
+            return pressed;
+        }
+        else if ((pressed & SCE_CTRL_UP) && *selection > 0)
+        {
+            // Move the current selection up
+            (*selection)--;
+        }
+        else if ((pressed & SCE_CTRL_DOWN) && *selection < items->size() - 1)
+        {
+            // Move the current selection down
+            (*selection)++;
+        }
+
+        sceDisplayWaitVblankStart();
+    }
+}
+
+uint32_t message(std::string text, uint32_t buttonMask)
+{
+    // Ignore any buttons that were already pressed
+    uint32_t buttons = 0xFFFFFFFF;
+
+    while (true)
+    {
+        vita2d_start_drawing();
+        vita2d_clear_screen();
+
+        size_t i = 0;
+        unsigned int y = 0;
+
+        // Draw the text, handling newline characters appropriately
+        while (true)
+        {
+            size_t j = text.find("\n", i);
+            vita2d_pgf_draw_text(pgf, 5, (y += 20), COLOR_TEXT1, 1.0f, text.substr(i, j - i).c_str());
+            if (j == std::string::npos) break;
+            i = j + 1;
+        }
+
+        vita2d_end_drawing();
+        vita2d_swap_buffers();
+
+        // Scan for newly-pressed buttons
+        SceCtrlData held;
+        sceCtrlPeekBufferPositive(0, &held, 1);
+        uint32_t pressed = held.buttons & ~buttons;
+        buttons = held.buttons;
+
+        // Return the pressed buttons so they can be handled
+        if (pressed & buttonMask)
+            return pressed;
+
+        sceDisplayWaitVblankStart();
+    }
+}
+
 void fileBrowser()
 {
+    ndsPath = gbaPath = "";
+
     std::string path = "ux0:";
-    std::string ndsPath, gbaPath;
     unsigned int selection = 0;
-    uint32_t buttons = 0;
 
     while (true)
     {
@@ -125,73 +246,34 @@ void fileBrowser()
         sceIoDclose(dir);
         sort(files.begin(), files.end());
 
-        vita2d_start_drawing();
-        vita2d_clear_screen();
+        // Show the file browser
+        uint32_t pressed = menu("NooDS", path.c_str(), &files, &selection, confirmButton | cancelButton);
 
-        // Draw the title and current path
-        vita2d_pgf_draw_text(pgf, 5, 20, COLOR_TEXT1, 1.0f, "NooDS");
-        vita2d_pgf_draw_text(pgf, 5, 40, COLOR_TEXT2, 1.0f, path.c_str());
-
-        // Draw the list of files at the current path
-        for (size_t i = 0; i < files.size(); i++)
-            vita2d_pgf_draw_text(pgf, 5, 80 + i * 20, (selection == i ? COLOR_TEXT3 : COLOR_TEXT1), 1.0f, files[i].c_str());
-
-        vita2d_end_drawing();
-        vita2d_swap_buffers();
-
-        while (true)
+        // Handle special menu input
+        if ((pressed & confirmButton) && files.size() > 0)
         {
-            // Scan for newly-pressed buttons
-            SceCtrlData held;
-            sceCtrlPeekBufferPositive(0, &held, 1);
-            uint32_t pressed = held.buttons & ~buttons;
-            buttons = held.buttons;
-
-            if ((pressed & confirmButton) && files.size() > 0)
+            if (files[selection].find(".nds", files[selection].length() - 4) != std::string::npos)
             {
-                // Handle the current selection
-                if (files[selection].find(".nds", files[selection].length() - 4) != std::string::npos)
-                {
-                    // Set an NDS ROM to load
-                    ndsPath = path + "/" + files[selection];
-                }
-                else if (files[selection].find(".gba", files[selection].length() - 4) != std::string::npos)
-                {
-                    // Set a GBA ROM to load
-                    gbaPath = path + "/" + files[selection];
-                }
-                else
-                {
-                    // Navigate to the selected directory
-                    path += "/" + files[selection];
-                    selection = 0;
-                }
-                break;
+                // Set an NDS ROM to load
+                ndsPath = path + "/" + files[selection];
             }
-
-            if ((pressed & cancelButton) && path != "ux0:")
+            else if (files[selection].find(".gba", files[selection].length() - 4) != std::string::npos)
             {
-                // Navigate to the previous directory
-                path = path.substr(0, path.rfind("/"));
+                // Set a GBA ROM to load
+                gbaPath = path + "/" + files[selection];
+            }
+            else
+            {
+                // Navigate to the selected directory
+                path += "/" + files[selection];
                 selection = 0;
-                break;
             }
-
-            if ((pressed & SCE_CTRL_UP) && selection > 0)
-            {
-                // Move the current selection up
-                selection--;
-                break;
-            }
-
-            if ((pressed & SCE_CTRL_DOWN) && selection < files.size() - 1)
-            {
-                // Move the current selection down
-                selection++;
-                break;
-            }
-
-            sceDisplayWaitVblankStart();
+        }
+        else if ((pressed & cancelButton) && path != "ux0:")
+        {
+            // Navigate to the previous directory
+            path = path.substr(0, path.rfind("/"));
+            selection = 0;
         }
 
         // Try to load a ROM if one was set
@@ -199,40 +281,160 @@ void fileBrowser()
         {
             try
             {
-                // Prepare the core and close the file browser
+                // Restart the core and close the menu
+                if (core) delete core;
                 core = new Core(ndsPath, gbaPath);
-                break;
+                return;
             }
             catch (int e)
             {
-                vita2d_start_drawing();
-                vita2d_clear_screen();
-
                 // Loading probably failed because of missing BIOS/firmware, so inform the user
-                vita2d_pgf_draw_text(pgf, 5, 20, COLOR_TEXT1, 1.0f, "Initialization failed.");
-                vita2d_pgf_draw_text(pgf, 5, 40, COLOR_TEXT1, 1.0f, "Make sure the path settings point to valid BIOS and firmware files and try again.");
-                vita2d_pgf_draw_text(pgf, 5, 60, COLOR_TEXT1, 1.0f, "You can modify the path settings in ux0:/data/noods/noods.ini.");
-
-                vita2d_end_drawing();
-                vita2d_swap_buffers();
-
-                while (true)
-                {
-                    // Scan for newly-pressed buttons
-                    SceCtrlData held;
-                    sceCtrlPeekBufferPositive(0, &held, 1);
-                    uint32_t pressed = held.buttons & ~buttons;
-                    buttons = held.buttons;
-
-                    // Stay on the error screen until dismissed
-                    if (pressed & confirmButton)
-                        break;
-
-                    sceDisplayWaitVblankStart();
-                }
+                std::string text =
+                    "Initialization failed.\n"
+                    "Make sure the path settings point to valid BIOS and firmware files and try again.\n"
+                    "You can modify the path settings in ux0:/data/noods/noods.ini.";
+                message(text, confirmButton);
 
                 ndsPath = gbaPath = "";
             }
+        }
+    }
+}
+
+void saveTypeMenu()
+{
+    unsigned int selection = 0;
+
+    std::vector<std::string> items;
+    if (core->isGbaMode())
+    {
+        // Set up list items for GBA save types
+        items.push_back("None");
+        items.push_back("EEPROM 0.5KB");
+        items.push_back("EEPROM 8KB");
+        items.push_back("SRAM 32KB");
+        items.push_back("FLASH 64KB");
+        items.push_back("FLASH 128KB");
+    }
+    else
+    {
+        // Set up list items for NDS save types
+        items.push_back("None");
+        items.push_back("EEPROM 0.5KB");
+        items.push_back("EEPROM 8KB");
+        items.push_back("EEPROM 64KB");
+        items.push_back("EEPROM 128KB");
+        items.push_back("FRAM 32KB");
+        items.push_back("FLASH 256KB");
+        items.push_back("FLASH 512KB");
+        items.push_back("FLASH 1024KB");
+        items.push_back("FLASH 8192KB");
+    }
+
+    while (true)
+    {
+        // Show the save type menu
+        uint32_t pressed = menu("Change Save Type", "", &items, &selection, confirmButton | cancelButton);
+
+        // Handle special menu input
+        if (pressed & confirmButton)
+        {
+            // Confirm the change because accidentally resizing a working save file could be bad!
+            if (!(message("Are you sure? This may result in data loss!", confirmButton | cancelButton) & confirmButton))
+                continue;
+
+            // Apply the change
+            if (core->isGbaMode())
+            {
+                switch (selection)
+                {
+                    case 0: core->cartridge.resizeGbaSave(0);       break; // None
+                    case 1: core->cartridge.resizeGbaSave(0x200);   break; // EEPROM 0.5KB
+                    case 2: core->cartridge.resizeGbaSave(0x2000);  break; // EEPROM 8KB
+                    case 3: core->cartridge.resizeGbaSave(0x8000);  break; // SRAM 32KB
+                    case 4: core->cartridge.resizeGbaSave(0x10000); break; // FLASH 64KB
+                    case 5: core->cartridge.resizeGbaSave(0x20000); break; // FLASH 128KB
+                }
+            }
+            else
+            {
+                switch (selection)
+                {
+                    case 0: core->cartridge.resizeNdsSave(0);        break; // None
+                    case 1: core->cartridge.resizeNdsSave(0x200);    break; // EEPROM 0.5KB
+                    case 2: core->cartridge.resizeNdsSave(0x2000);   break; // EEPROM 8KB
+                    case 3: core->cartridge.resizeNdsSave(0x10000);  break; // EEPROM 64KB
+                    case 4: core->cartridge.resizeNdsSave(0x20000);  break; // EEPROM 128KB
+                    case 5: core->cartridge.resizeNdsSave(0x8000);   break; // FRAM 32KB
+                    case 6: core->cartridge.resizeNdsSave(0x40000);  break; // FLASH 256KB
+                    case 7: core->cartridge.resizeNdsSave(0x80000);  break; // FLASH 512KB
+                    case 8: core->cartridge.resizeNdsSave(0x100000); break; // FLASH 1024KB
+                    case 9: core->cartridge.resizeNdsSave(0x800000); break; // FLASH 8192KB
+                }
+            }
+
+            // Restart the core and close the menu
+            if (core) delete core;
+            core = new Core(ndsPath, gbaPath);
+            return;
+        }
+        else if (pressed & cancelButton)
+        {
+            // Close the menu
+            return;
+        }
+    }
+}
+
+void pauseMenu()
+{
+    stopCore();
+
+    unsigned int selection = 0;
+
+    std::vector<std::string> items =
+    {
+        "Resume",
+        "Restart",
+        "Change Save Type",
+        "File Browser"
+    };
+
+    while (true)
+    {
+        // Show the pause menu
+        uint32_t pressed = menu("NooDS", "", &items, &selection, confirmButton | cancelButton);
+
+        // Handle special menu input
+        if (pressed & confirmButton)
+        {
+            switch (selection)
+            {
+                case 0: // Resume
+                    startCore();
+                    return;
+
+                case 1: // Restart
+                    if (core) delete core;
+                    core = new Core(ndsPath, gbaPath);
+                    startCore();
+                    return;
+
+                case 2: // Change Save Type
+                    saveTypeMenu();
+                    break;
+
+                case 3: // File Browser
+                    fileBrowser();
+                    startCore();
+                    return;
+            }
+        }
+        else if (pressed & cancelButton)
+        {
+            // Resume and close the menu
+            startCore();
+            return;
         }
     }
 }
@@ -294,6 +496,10 @@ int main()
     confirmButton = (assign ? SCE_CTRL_CROSS  : SCE_CTRL_CIRCLE);
     cancelButton  = (assign ? SCE_CTRL_CIRCLE : SCE_CTRL_CROSS);
 
+    // Set up button and touch controls
+    sceCtrlSetSamplingMode(SCE_CTRL_MODE_ANALOG);
+    sceTouchSetSamplingState(SCE_TOUCH_PORT_FRONT, SCE_TOUCH_SAMPLING_STATE_START);
+
     // Initialize graphics and textures
     vita2d_init();
     vita2d_set_clear_color(COLOR_CLEAR);
@@ -306,25 +512,25 @@ int main()
     vita2d_texture_set_filters(top, filter, filter);
     vita2d_texture_set_filters(bot, filter, filter);
 
+    // Initialize audio output
+    audioPort = sceAudioOutOpenPort(SCE_AUDIO_OUT_PORT_TYPE_BGM, 1024, 48000, SCE_AUDIO_OUT_MODE_STEREO);
+
     // Open the file browser
     fileBrowser();
 
-    // Set up touch sampling and audio output
-    sceTouchSetSamplingState(SCE_TOUCH_PORT_FRONT, SCE_TOUCH_SAMPLING_STATE_START);
-    audioPort = sceAudioOutOpenPort(SCE_AUDIO_OUT_PORT_TYPE_BGM, 1024, 48000, SCE_AUDIO_OUT_MODE_STEREO);
-
-    // Start the core
-    std::thread coreThread(runCore);
-    std::thread audioThread(outputAudio);
-    std::thread saveThread(checkSave);
-
+    // Set the screen layout and start the core
     layout.update(960, 544, gbaMode);
+    startCore();
 
     while (true)
     {
         // Scan for button input
         SceCtrlData pressed;
         sceCtrlPeekBufferPositive(0, &pressed, 1);
+
+        // Open the pause menu if the right stick is flicked down
+        if (pressed.ry >= 192)
+            pauseMenu();
 
         // Send input to the core
         for (int i = 0; i < 12; i++)
