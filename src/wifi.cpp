Hydr8gon/NooDS

commit 50844e8fa8fd1b866f156f1e0a8782a736d637a5

Minor fixes and cleanups

src/wifi.cpp
@@ -143,7 +143,7 @@ void Wifi::processPackets()
             {
                 wRxbufWrcsr++;
                 wRxbufWrcsr = (wRxbufBegin & 0x1FFE) + (wRxbufWrcsr - (wRxbufBegin & 0x1FFE)) % ((wRxbufEnd & 0x1FFE) - (wRxbufBegin & 0x1FFE));
-                wRxbufWrcsr &= 0x1FFF;
+                wRxbufWrcsr &= 0xFFF;
             }
         }
 
@@ -344,7 +344,7 @@ void Wifi::writeWTxbufWrData(uint16_t mask, uint16_t value)
     wTxbufWrAddr += 2;
     if (wTxbufWrAddr == wTxbufGap)
         wTxbufWrAddr += wTxbufGapdisp << 1;
-    wTxbufWrAddr %= 0x2000;
+    wTxbufWrAddr &= 0x1FFF;
 
     // Decrement the write counter and trigger an interrupt at the end
     if (wTxbufCount > 0 && --wTxbufCount == 0)
@@ -503,12 +503,14 @@ uint16_t Wifi::readWRxbufRdData()
     uint16_t value = core->memory.read<uint16_t>(1, 0x4804000 + wRxbufRdAddr);
 
     // Increment the read address
-    wRxbufRdAddr += 2;
-    if (wRxbufRdAddr == wRxbufGap)
-        wRxbufRdAddr += wRxbufGapdisp << 1;
     if ((wRxbufBegin & 0x1FFE) != (wRxbufEnd & 0x1FFE))
+    {
+        wRxbufRdAddr += 2;
+        if (wRxbufRdAddr == wRxbufGap)
+            wRxbufRdAddr += wRxbufGapdisp << 1;
         wRxbufRdAddr = (wRxbufBegin & 0x1FFE) + (wRxbufRdAddr - (wRxbufBegin & 0x1FFE)) % ((wRxbufEnd & 0x1FFE) - (wRxbufBegin & 0x1FFE));
-    wRxbufRdAddr %= 0x2000;
+        wRxbufRdAddr &= 0x1FFF;
+    }
 
     // Decrement the read counter and trigger an interrupt at the end
     if (wRxbufCount > 0 && --wRxbufCount == 0)

src/cartridge.cpp
@@ -44,9 +44,9 @@ bool Cartridge::loadRom(std::string path)
     romSize = ftell(romFile);
     fseek(romFile, 0, SEEK_SET);
 
-    // Choose the save extension based on the instance number
+    // Choose the save extension based on the instance ID
     // This ensures multiple instances don't write over the same file
-    std::string ext = number ? (".sv" + std::to_string(number + 1)) : ".sav";
+    std::string ext = core->getId() ? (".sv" + std::to_string(core->getId() + 1)) : ".sav";
 
     // Attempt to load the ROM's save into memory
     saveName = path.substr(0, path.rfind(".")) + ext;

src/cartridge.h
@@ -38,7 +38,7 @@ enum NdsCmdMode
 class Cartridge
 {
     public:
-        Cartridge(Core *core, int number): core(core), number(number) {}
+        Cartridge(Core *core): core(core) {}
         ~Cartridge();
 
         virtual bool loadRom(std::string path);
@@ -52,7 +52,6 @@ class Cartridge
 
     protected:
         Core *core;
-        int number;
 
         FILE *romFile = nullptr;
         uint8_t *rom = nullptr, *save = nullptr;
@@ -69,7 +68,7 @@ class Cartridge
 class CartridgeNds: public Cartridge
 {
     public:
-        CartridgeNds(Core *core, int number): Cartridge(core, number) {}
+        CartridgeNds(Core *core): Cartridge(core) {}
 
         bool loadRom(std::string path);
         void directBoot();
@@ -115,7 +114,7 @@ class CartridgeNds: public Cartridge
 class CartridgeGba: public Cartridge
 {
     public:
-        CartridgeGba(Core *core, int number): Cartridge(core, number) {}
+        CartridgeGba(Core *core): Cartridge(core) {}
 
         bool loadRom(std::string path);
 

src/core.cpp
@@ -24,9 +24,10 @@
 #include "core.h"
 #include "settings.h"
 
-Core::Core(std::string ndsPath, std::string gbaPath, int number):
-    cartridgeNds(this, number),
-    cartridgeGba(this, number),
+Core::Core(std::string ndsPath, std::string gbaPath, int id):
+    id(id),
+    cartridgeNds(this),
+    cartridgeGba(this),
     cp15(this),
     divSqrt(this),
     dldi(this),
@@ -40,7 +41,7 @@ Core::Core(std::string ndsPath, std::string gbaPath, int number):
     ipc(this),
     memory(this),
     rtc(this),
-    spi(this, number),
+    spi(this),
     spu(this),
     timers { Timers(this, 0), Timers(this, 1) },
     wifi(this)

src/core.h
@@ -66,11 +66,12 @@ struct Task
 class Core
 {
     public:
-        Core(std::string ndsPath = "", std::string gbaPath = "", int number = 0);
+        Core(std::string ndsPath = "", std::string gbaPath = "", int id = 0);
 
         void runFrame() { (this->*runFunc)(); }
 
         bool isGbaMode() { return gbaMode; }
+        int  getId()     { return id;      }
         int  getFps()    { return fps;     }
 
         uint32_t getGlobalCycles() { return globalCycles; }
@@ -100,8 +101,9 @@ class Core
         Wifi wifi;
 
     private:
-        bool gbaMode = false;
         void (Core::*runFunc)() = &Core::runNdsFrame;
+        bool gbaMode = false;
+        int id = 0;
 
         std::vector<Task> tasks;
         uint32_t globalCycles = 0;

src/desktop/layout_dialog.cpp
@@ -63,7 +63,7 @@ EVT_BUTTON(wxID_CANCEL,       LayoutDialog::cancel)
 EVT_BUTTON(wxID_OK,           LayoutDialog::confirm)
 wxEND_EVENT_TABLE()
 
-LayoutDialog::LayoutDialog(NooFrame *frame): wxDialog(nullptr, wxID_ANY, "Screen Layout"), frame(frame)
+LayoutDialog::LayoutDialog(NooApp *app): wxDialog(nullptr, wxID_ANY, "Screen Layout"), app(app)
 {
     // Remember the previous settings in case the changes are discarded
     prevSettings[0] = ScreenLayout::getScreenRotation();
@@ -173,144 +173,112 @@ void LayoutDialog::rotateNone(wxCommandEvent &event)
 {
     // Set the screen rotation setting to none
     ScreenLayout::setScreenRotation(0);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::rotateCw(wxCommandEvent &event)
 {
     // Set the screen rotation setting to clockwise
     ScreenLayout::setScreenRotation(1);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::rotateCcw(wxCommandEvent &event)
 {
     // Set the screen rotation setting to counter-clockwise
     ScreenLayout::setScreenRotation(2);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::arrangeAuto(wxCommandEvent &event)
 {
     // Set the screen arrangement setting to automatic
     ScreenLayout::setScreenArrangement(0);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::arrangeVert(wxCommandEvent &event)
 {
     // Set the screen arrangement setting to vertical
     ScreenLayout::setScreenArrangement(1);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::arrangeHori(wxCommandEvent &event)
 {
     // Set the screen arrangement setting to horizontal
     ScreenLayout::setScreenArrangement(2);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::sizeEven(wxCommandEvent &event)
 {
     // Set the screen sizing setting to even
     ScreenLayout::setScreenSizing(0);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::sizeTop(wxCommandEvent &event)
 {
     // Set the screen sizing setting to enlarge top
     ScreenLayout::setScreenSizing(1);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::sizeBot(wxCommandEvent &event)
 {
     // Set the screen sizing setting to enlarge bottom
     ScreenLayout::setScreenSizing(2);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::gapNone(wxCommandEvent &event)
 {
     // Set the screen gap setting to none
     ScreenLayout::setScreenGap(0);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::gapQuart(wxCommandEvent &event)
 {
     // Set the screen gap setting to quarter
     ScreenLayout::setScreenGap(1);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::gapHalf(wxCommandEvent &event)
 {
     // Set the screen gap setting to half
     ScreenLayout::setScreenGap(2);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::gapFull(wxCommandEvent &event)
 {
     // Set the screen gap setting to full
     ScreenLayout::setScreenGap(3);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::intScale(wxCommandEvent &event)
 {
     // Toggle the integer scale setting
     ScreenLayout::setIntegerScale(!ScreenLayout::getIntegerScale());
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::gbaCrop(wxCommandEvent &event)
 {
     // Toggle the GBA crop setting
     ScreenLayout::setGbaCrop(!ScreenLayout::getGbaCrop());
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::filter(wxCommandEvent &event)
 {
     // Toggle the screen filter setting
     NooApp::setScreenFilter(!NooApp::getScreenFilter());
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 }
 
 void LayoutDialog::cancel(wxCommandEvent &event)
@@ -323,9 +291,7 @@ void LayoutDialog::cancel(wxCommandEvent &event)
     ScreenLayout::setIntegerScale(prevSettings[4]);
     ScreenLayout::setGbaCrop(prevSettings[5]);
     NooApp::setScreenFilter(prevSettings[6]);
-
-    // Trigger a resize to update the screen layout
-    frame->SendSizeEvent();
+    app->updateLayouts();
 
     event.Skip(true);
 }

src/desktop/layout_dialog.h
@@ -22,15 +22,15 @@
 
 #include <wx/wx.h>
 
-#include "noo_frame.h"
+class NooApp;
 
 class LayoutDialog: public wxDialog
 {
     public:
-        LayoutDialog(NooFrame *frame);
+        LayoutDialog(NooApp *app);
 
     private:
-        NooFrame *frame;
+        NooApp *app;
 
         int prevSettings[7];
 

src/desktop/noo_app.cpp
@@ -106,7 +106,7 @@ bool NooApp::OnInit()
 
 void NooApp::createFrame()
 {
-    // Create a new frame using the lowest free instance number
+    // Create a new frame using the lowest free instance ID
     for (int i = 0; i < MAX_FRAMES; i++)
     {
         if (!frames[i])
@@ -117,31 +117,41 @@ void NooApp::createFrame()
     }
 }
 
-void NooApp::removeFrame(int number)
+void NooApp::removeFrame(int id)
 {
-    // Free an instance number; this should be done on frame destruction
-    frames[number] = nullptr;
+    // Free an instance ID; this should be done on frame destruction
+    frames[id] = nullptr;
 }
 
-void NooApp::connectCore(int number)
+void NooApp::connectCore(int id)
 {
     // Connect a frame's core to all other active cores
     for (int i = 0; i < MAX_FRAMES; i++)
     {
-        if (!frames[i] || i == number) continue;
+        if (!frames[i] || i == id) continue;
         if (Core *core = frames[i]->getCore())
-            core->wifi.addConnection(frames[number]->getCore());
+            core->wifi.addConnection(frames[id]->getCore());
     }
 }
 
-void NooApp::disconnCore(int number)
+void NooApp::disconnCore(int id)
 {
     // Disconnect a frame's core from all other active cores
     for (int i = 0; i < MAX_FRAMES; i++)
     {
-        if (!frames[i] || i == number) continue;
+        if (!frames[i] || i == id) continue;
         if (Core *core = frames[i]->getCore())
-            core->wifi.remConnection(frames[number]->getCore());
+            core->wifi.remConnection(frames[id]->getCore());
+    }
+}
+
+void NooApp::updateLayouts()
+{
+    // Trigger resize events for frames to update screen layouts
+    for (size_t i = 0; i < MAX_FRAMES; i++)
+    {
+        if (frames[i])
+            frames[i]->SendSizeEvent();
     }
 }
 
@@ -163,7 +173,7 @@ int NooApp::audioCallback(const void *in, void *out, unsigned long count,
     uint32_t *original = nullptr;
 
     // Get samples from each instance so frame limiting is enforced
-    // Only the lowest instance number's samples are played; the rest are discarded
+    // Only the lowest instance ID's samples are played; the rest are discarded
     for (size_t i = 0; i < MAX_FRAMES; i++)
     {
         if (!frames[i]) continue;

commit 1c19eca1fe5666430b930171f604d4f72cbed386

Fix WiFi buffer wraparound Fixes PictoChat freeze (regression from 50844e8)

src/wifi.cpp
@@ -136,15 +136,13 @@ void Wifi::processPackets()
         for (size_t j = 0; j < size; j++)
         {
             // Write a half-word of the packet to memory
-            core->memory.write<uint16_t>(1, 0x4804000 + (wRxbufWrcsr << 1), packets[i][j]);
+            core->memory.write<uint16_t>(1, 0x4804000 + wRxbufWrcsr, packets[i][j]);
 
             // Increment the circular buffer address
+            wRxbufWrcsr += 2;
             if ((wRxbufBegin & 0x1FFE) != (wRxbufEnd & 0x1FFE))
-            {
-                wRxbufWrcsr++;
                 wRxbufWrcsr = (wRxbufBegin & 0x1FFE) + (wRxbufWrcsr - (wRxbufBegin & 0x1FFE)) % ((wRxbufEnd & 0x1FFE) - (wRxbufBegin & 0x1FFE));
-                wRxbufWrcsr &= 0xFFF;
-            }
+            wRxbufWrcsr &= 0x1FFE;
         }
 
         delete[] packets[i];
@@ -242,7 +240,7 @@ void Wifi::writeWRxcnt(uint16_t mask, uint16_t value)
 
     // Latch W_RXBUF_WR_ADDR to W_RXBUF_WRCSR
     if (value & BIT(0))
-        wRxbufWrcsr = wRxbufWrAddr;
+        wRxbufWrcsr = wRxbufWrAddr << 1;
 }
 
 void Wifi::writeWPowerstate(uint16_t mask, uint16_t value)
@@ -503,14 +501,11 @@ uint16_t Wifi::readWRxbufRdData()
     uint16_t value = core->memory.read<uint16_t>(1, 0x4804000 + wRxbufRdAddr);
 
     // Increment the read address
+    if ((wRxbufRdAddr += 2) == wRxbufGap)
+        wRxbufRdAddr += wRxbufGapdisp << 1;
     if ((wRxbufBegin & 0x1FFE) != (wRxbufEnd & 0x1FFE))
-    {
-        wRxbufRdAddr += 2;
-        if (wRxbufRdAddr == wRxbufGap)
-            wRxbufRdAddr += wRxbufGapdisp << 1;
         wRxbufRdAddr = (wRxbufBegin & 0x1FFE) + (wRxbufRdAddr - (wRxbufBegin & 0x1FFE)) % ((wRxbufEnd & 0x1FFE) - (wRxbufBegin & 0x1FFE));
-        wRxbufRdAddr &= 0x1FFF;
-    }
+    wRxbufRdAddr &= 0x1FFF;
 
     // Decrement the read counter and trigger an interrupt at the end
     if (wRxbufCount > 0 && --wRxbufCount == 0)

src/wifi.h
@@ -49,7 +49,7 @@ class Wifi
         uint16_t readWPowerforce()        { return wPowerforce;      }
         uint16_t readWRxbufBegin()        { return wRxbufBegin;      }
         uint16_t readWRxbufEnd()          { return wRxbufEnd;        }
-        uint16_t readWRxbufWrcsr()        { return wRxbufWrcsr;      }
+        uint16_t readWRxbufWrcsr()        { return wRxbufWrcsr >> 1; }
         uint16_t readWRxbufWrAddr()       { return wRxbufWrAddr;     }
         uint16_t readWRxbufRdAddr()       { return wRxbufRdAddr;     }
         uint16_t readWRxbufReadcsr()      { return wRxbufReadcsr;    }